b7a21ae69e395b85c7c5b8e85ea8204435712b50
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-02-25 23:00:11 +0800

    feat: rebuild solx if static library deps are changed (#9)

--- solx-solc/build.rs
+++ solx-solc/build.rs
@@ -6,9 +6,17 @@
 /// Links solc and Boost libraries statically.
 ///
 fn main() {
+    // Re-run if the `Boost` path environment variable is changed.
+    println!("cargo:rerun-if-env-changed={}", env!("BOOST_PREFIX"));
+    // Re-run if the `solc` path environment variable is changed.
+    println!("cargo:rerun-if-env-changed={}", env!("SOLC_PREFIX"));
+    // Re-run if the `solc` directory contents are changed.
+    if let Ok(path) = std::env::var("SOLC_PREFIX") {
+        println!("cargo:rerun-if-changed={}", path);
+    }
+
     // Where to find Boost libraries.
     println!("cargo:rustc-link-search=native={}", env!("BOOST_PREFIX"));
-
     // Where to find solc libraries.
     for directory in [
         "libsolc",
@@ -25,15 +33,14 @@ fn main() {
         );
     }
 
-    // Link against the static libraries.
+    // Link with Boost libraries.
+    for library in ["boost_filesystem", "boost_system", "boost_program_options"] {
+        println!("cargo:rustc-link-lib=static={library}");
+    }
+    // Link with solc libraries.
     for library in [
         "solc", "solidity", "solutil", "langutil", "evmasm", "yul", "smtutil",
     ] {
-        println!("cargo:rustc-link-lib=static={library}");
-    }
-
-    // Link the Boost libraries.
-    for library in ["boost_filesystem", "boost_system", "boost_program_options"] {
         println!("cargo:rustc-link-lib=static={library}");
     }
 }
